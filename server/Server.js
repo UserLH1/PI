829719995821ac412bcf4e09455ab71cf40fa8ed
UserLH1 <lazeahoratiu@gmail.com>
2023-12-15 16:09:48 +0200

    added more components

--- server/Server.js
+++ server/Server.js
@@ -10,84 +10,85 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "pwd_mgr"
-})
-
-
-app.post('/addItem', (req, res) => {
-    console.log(user_id);
-    const sql = "INSERT INTO password (`user_id`, `name`, `URL`, `username`,`password`) VALUES (?, ?, ?, ?, ?)";
-    const values = [
-        user_id,
-        req.body.name,
-        req.body.URL,
-        req.body.username,
-        req.body.password
-    ];
-    
-    console.log(values);
-    
-    db.query(sql, values, (err, data) => {
-        if (err) {
-            console.log("Error:" + err.message);
-        }
-        return res.json(data);
-    });
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "pwd_mgr",
 });
 
-app.post('/login', (req, res) => {
-    const { email, password } = req.body;
-    const sql = "SELECT * FROM user WHERE `email` = ?";
-    user_id
-    db.query(sql, [email], (err, data) => {
-        user_id = data[0].id;
-        // console.log(user_id);
-        // console.log(data);
-        if (err) {
-            return res.status(500).json("Error");
-        }
-        if(data.length > 0) {
-            bcrypt.compare(password, data[0].password, (err, result) => {
-                if (result) {
-                    // Passwords match
-                    return res.json("Success");
-                } else {
-                    // Passwords don't match
-                    return res.json("Fail");
-                }
-            });
+app.post("/addItem", (req, res) => {
+  console.log(user_id);
+  const sql =
+    "INSERT INTO password (`user_id`, `name`, `URL`, `username`,`password`) VALUES (?, ?, ?, ?, ?)";
+  const values = [
+    user_id,
+    req.body.name,
+    req.body.URL,
+    req.body.username,
+    req.body.password,
+  ];
+
+  console.log(values);
+
+  db.query(sql, values, (err, data) => {
+    if (err) {
+      console.log("Error:" + err.message);
+    }
+    return res.json(data);
+  });
+});
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  const sql = "SELECT * FROM user WHERE `email` = ?";
+  user_id;
+  db.query(sql, [email], (err, data) => {
+    user_id = data[0].id;
+    // console.log(user_id);
+    // console.log(data);
+    if (err) {
+      return res.status(500).json("Error");
+    }
+    if (data.length > 0) {
+      bcrypt.compare(password, data[0].password, (err, result) => {
+        if (result) {
+          // Passwords match
+          return res.json("Success");
         } else {
-            return res.json("Fail");
+          // Passwords don't match
+          return res.json("Fail");
         }
-    });
+      });
+    } else {
+      return res.json("Fail");
+    }
+  });
 });
 
-app.post('/register', (req, res) => {
-    const { username, email, password } = req.body;
+app.post("/register", (req, res) => {
+  const { username, email, password } = req.body;
 
-    // Hash the password before storing it
-    bcrypt.hash(password, saltRounds, (err, hash) => {
-        if (err) {
-            console.log("Error hashing password:" + err.message);
-            return res.json(err.message);
-        }
+  // Hash the password before storing it
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    if (err) {
+      console.log("Error hashing password:" + err.message);
+      return res.json(err.message);
+    }
 
-        const sql = "INSERT INTO user (`username`, `email`, `password`) VALUES (?, ?, ?)";
-        const values = [username, email, hash]; // Use the hash instead of the password
+    const sql =
+      "INSERT INTO user (`username`, `email`, `password`) VALUES (?, ?, ?)";
+    const values = [username, email, hash]; // Use the hash instead of the password
 
-        db.query(sql, values, (err, data) => {
-            if (err) {
-                console.log("Error:" + err.message);
-                return res.json(err.message);
-            }
-            return res.json("Success");
-        });
+    db.query(sql, values, (err, data) => {
+      if (err) {
+        console.log("Error:" + err.message);
+        return res.json(err.message);
+      }
+      return res.json("Success");
     });
+  });
 });
 
-app.listen(8080, () =>{
-    console.log("listenting");
-})
+app.listen(8080, () => {
+  console.log("listenting");
+});
