5f0da40dfa6e4ba0d05e9094d6f919f4ff1fdee6
UserLH1 <lazeahoratiu@gmail.com>
2024-01-06 23:58:02 +0200

    added styles for the Password generator

--- client/src/Pages/PasswordGenerator.jsx
+++ client/src/Pages/PasswordGenerator.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
-import "../Styles/password-generator.css";
-import copyIcon from "../Assets/copy-icon.svg";
 import { ToastContainer, toast } from "react-toastify";
+import zxcvbn from "zxcvbn";
+import copyIcon from "../Assets/copy-icon.svg";
 import Header from "../Components/Header";
-import zxcvbn from 'zxcvbn';
+import "../Styles/password-generator.css";
 
 const lowercaseList = "abcdefghijklmnopqrstuvwxyz";
 const uppercaseList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -25,7 +25,7 @@ function PasswordGenerator() {
   ]);
   const [passwordStrength, setPasswordStrength] = useState({
     score: 0,
-    timeToCrack: '',
+    timeToCrack: "",
   });
 
   useEffect(() => {
@@ -70,9 +70,27 @@ function PasswordGenerator() {
 
     setPassword(tempPassword);
     const strength = zxcvbn(password);
+    console.log(strength);
+    const createPassLabel = () => {
+      switch (strength.score) {
+        case 0:
+          return `Very weak`;
+        case 1:
+          return `Weak`;
+        case 2:
+          return `Medium`;
+        case 3:
+          return `Strong`;
+        case 4:
+          return `Very Strong`;
+        default:
+          return `none`;
+      }
+    };
     setPasswordStrength({
-      score: strength.score,
-      timeToCrack: strength.crack_times_display.offline_slow_hashing_1e4_per_second,
+      score: createPassLabel,
+      timeToCrack:
+        strength.crack_times_display.offline_slow_hashing_1e4_per_second,
     });
   };
 
@@ -88,25 +106,32 @@ function PasswordGenerator() {
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
-       
       });
     }
   };
-useEffect(() => {
-  generatePassword();
-}, [lowerCase, upperCase, numbers, symbols, passwordLength]);
+  useEffect(() => {
+    generatePassword();
+  }, [lowerCase, upperCase, numbers, symbols, passwordLength]);
 
   return (
     <>
       <Header />
-      
-      <div className="container-custom">
-      <div className="password-strength-container">
-        <h2 className="title">Strong Password Generator</h2>
-        <span className="password-score">Your password's score: {passwordStrength.score}</span>
-        <span className="time-to-crack">Estimated time to crack: {passwordStrength.timeToCrack}</span>
- 
+      <div className="page-heading-container">
+        <h1 className="page-heading">Password Generator</h1>
+        <p className="subheading">
+          Create complex passwords to keep your information safe.
+        </p>
       </div>
+
+      <div className="container-custom">
+        <div className="password-strength-container">
+          <span className="password-score">
+            Your password's score: {passwordStrength.score}
+          </span>
+          <span className="time-to-crack">
+            Estimated time to crack: {passwordStrength.timeToCrack}
+          </span>
+        </div>
         <div className="password-wrapper">
           <div className="password-area">
             <div className="password">
@@ -125,14 +150,20 @@ useEffect(() => {
             </div>
           </div>
         </div>
+        <div className="buttons">
+          <button type="button" onClick={copyPassword}>
+            Copy Password
+          </button>
+          <button type="button" onClick={generatePassword}>
+            Regenerate
+          </button>
+        </div>
         <div className="setting">
-          <h3>Customize your password</h3>
           <div className="customize">
             <div className="checkboxes">
               <div className="left">
                 <div className="checkbox-field">
                   <input
-                    
                     type="checkbox"
                     name="lower"
                     id="lower"
@@ -151,7 +182,6 @@ useEffect(() => {
                 </div>
                 <div className="checkbox-field">
                   <input
-                  
                     type="checkbox"
                     name="upper"
                     id="upper"
@@ -172,8 +202,6 @@ useEffect(() => {
               <div className="right">
                 <div className="checkbox-field">
                   <input
-                   
-                    
                     type="checkbox"
                     name="numbers"
                     id="numbers"
@@ -192,7 +220,6 @@ useEffect(() => {
                 </div>
                 <div className="checkbox-field">
                   <input
-                   
                     type="checkbox"
                     name="symbols"
                     id="symbols"
@@ -213,7 +240,7 @@ useEffect(() => {
             </div>
           </div>
         </div>
-        <div className="password-length">
+        <span className="password-length">
           <h3>Password Length</h3>
           <div className="slider">
             <p className="rangeValue">{passwordLength}</p>
@@ -229,15 +256,8 @@ useEffect(() => {
               />
             </div>
           </div>
-        </div>
-        <div className="buttons">
-          <button type="button" onClick={copyPassword}>
-            Copy Password
-          </button>
-          <button type="button" onClick={generatePassword}>
-            Generate Password
-          </button>
-        </div>
+        </span>
+
         <ToastContainer />
       </div>
     </>
